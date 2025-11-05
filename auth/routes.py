061ebb90fc8e622bff152add4903b939e72304a1
Maua Imani <imanimaua@gmail.com>
2025-11-05 17:11:51 +0100

    Added invite_user endpoint

--- auth/routes.py
+++ auth/routes.py
@@ -32,3 +32,38 @@ async def create_user(user: Annotated[createUser, Body()],
     id = row["id"]
     return UserInvite(sub=id,username=username, email=user.email, role=user.user_role, password=temporary) 
 
+INVITE_EXPIRY_HOURS = 24
+@auth_router.post("/send_invite", response_model=dict)
+async def invite_user(user_id:Annotated[sendRequest, Body()], 
+                      db: Annotated[asyncpg.Connection, Depends(get_db)],
+                      _: str = Depends(required_roles(UserRole.superuser, UserRole.admin))):
+    user: UserInDB = await get_user(db=db, id=user_id.user_id)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="User does not exist")
+    try:        
+        payload = TokenPayload(sub=user.username, id=user.id, email=user.email, role=user.user_role, type="invite")
+    except:
+        raise ValueError("Incorrect data type")
+    invite_token_expires = timedelta(hours=INVITE_EXPIRY_HOURS)
+    invite_token: InviteToken = create_jwt(data=payload, expiry=invite_token_expires)
+    stored_invite = await store_token(data=payload, jwt=invite_token, db=db)
+
+    invite_link = f"https://shiftly.app/register?token={invite_token}"
+    name = user.username.split(".")[0].capitalize()
+
+    subject = "You've been invited to Shiftly!"
+    body = f""" 
+Hello {name},
+You’ve been invited to join Shiftly as a {user.user_role}.
+
+    Registration link (valid {INVITE_EXPIRY_HOURS} h): {invite_link}
+
+    Please login and change your password immediately after first access.
+    """
+
+    try:
+        send_email(to_email=user.email, subject=subject, body=body)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Failed to send email: {e}")
+    
+    return {"message": f"Invite sent to {user.email}"}
